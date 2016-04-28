5edd40e35ac0e3437d52faefac3da5e1f11805bd
123 <123@DESKTOP-VR28GMB>
2016-04-28 06:59:02 +0300

    Add project files.

---
+++ Encode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
